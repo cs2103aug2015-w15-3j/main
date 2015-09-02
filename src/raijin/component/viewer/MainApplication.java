6c59a4309c2de2005fe1d83a1f23e6dd2a7934c7
jinifief <jinified@gmail.com>
2015-09-02 17:05:26 +0800

    Added JavaFX class

---
+++ src/raijin/component/viewer/MainApplication.java
@@ -0,0 +1,14 @@
+package raijin.component.viewer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
