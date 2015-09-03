336d7338bf0942f6f31c726c539fdb02550a1d4d
jinifief <jinified@gmail.com>
2015-09-04 04:51:41 +0800

    Boilerplate for JavaFX Application

--- src/raijin/component/viewer/MainApplication.java
+++ src/raijin/component/viewer/MainApplication.java
@@ -1,14 +1,40 @@
 package raijin.component.viewer;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
-    // TODO Auto-generated method stub
-    
+  public void start(Stage stage) throws Exception {
+    Button btn = new Button();
+    btn.setText("Say 'Hello, World!");
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+      
+      @Override
+      public void handle(ActionEvent actionEvent){
+        System.out.println("Hello, World!");
+      }
+    });
+
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+    Scene scene = new Scene(root,300,250);
+    stage.setTitle("Welcome to Raijin");
+    stage.setScene(scene);
+    stage.show();
+  }
+  
+  
+  public static void main(String[] args) {
+    launch(args);
   }
 
 }
