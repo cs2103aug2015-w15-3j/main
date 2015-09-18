2290611b37b1245dcb69c3a3c36bd00b4a0ee3c6
jinifief <jinified@gmail.com>
2015-09-18 23:04:18 +0800

    Tested first time login

--- src/raijin/logic/api/Logic.java
+++ src/raijin/logic/api/Logic.java
@@ -1,11 +1,55 @@
 package raijin.logic.api;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Path;
+
+import raijin.common.datatypes.Constants;
 import raijin.common.datatypes.Status;
+import raijin.storage.api.History;
+import raijin.storage.api.Memory;
+import raijin.storage.handler.StorageHandler;
 
 
+/**
+ * 
+ * @author papa
+ * Basically a facade class that handles all components needs
+ */
 public class Logic {
 
+  private Memory memory;
+  private History history;
+  private CommandDispatcher commandDispatcher;
+  private String currentDirectory;              //Records down current directory
+  
+  public Logic() {
+    setupEnvironment();
+  }
   public Status handleInput(String userInput) {
     return null;
   }
+  
+  private void setupEnvironment() {
+    memory = Memory.getMemory();
+    history = History.getHistory();
+    //commandDispatcher = CommandDispatcher.getDispatcher();
+    try {
+      currentDirectory = StorageHandler.getJarPath();
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  /*Checks if this is the first time a user use the program*/
+  public boolean isFirstTime() {
+    //@TODO ensures that the directory path works with Windows
+    String dataPath = currentDirectory+Constants.NAME_USER_FOLDER;
+    return !StorageHandler.isDirectory(dataPath);
+  }
+
+  /*Used for testing purpose or in case when user wants to change location of storage*/
+  public void setCurrentPath(String path) {
+    currentDirectory = path;
+  }
+
 }
--- test/raijin/logic/api/LogicTest.java
+++ test/raijin/logic/api/LogicTest.java
@@ -3,16 +3,26 @@ package raijin.logic.api;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class LogicTest {
 
+  private Logic logic;
+
+  @Rule public TemporaryFolder tmpFolder = new TemporaryFolder();
+
   @Before
-  public void setUp() throws Exception {}
+  public void setUp() throws Exception {
+    logic = new Logic();
+  }
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void isFirstTime_FirstTime_CreateDirectory() {
+    //Sets path to temporary path for testing
+    logic.setCurrentPath(tmpFolder.getRoot().getAbsolutePath());
+    assertTrue(logic.isFirstTime());
   }
 
 }
