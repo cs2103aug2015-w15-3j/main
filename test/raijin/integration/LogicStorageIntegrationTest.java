8768b3f6323c9c0db6f5cb8dcdb9ac4e3751de6a
jinifief <jinified@gmail.com>
2015-09-19 15:45:10 +0800

    Deleted Session class

---
+++ test/raijin/integration/LogicStorageIntegrationTest.java
@@ -0,0 +1,18 @@
+package raijin.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LogicStorageIntegrationTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
