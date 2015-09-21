a1ff1cf3d29e14c325fe8a345a4cd0786d68c278
jinifief <jinified@gmail.com>
2015-09-21 15:12:27 +0800

    Refactored ParsedInput

--- src/raijin/logic/parser/ParsedInput.java
+++ src/raijin/logic/parser/ParsedInput.java
@@ -10,17 +10,100 @@ import raijin.common.datatypes.Status;
  */
 public class ParsedInput {
   
-  private Constants.Command command;
+  //===========================================================================
+  // Properties of Parsed Input 
+  //===========================================================================
   
-  /*Parser will output ParsedInput with user command*/
-  public ParsedInput(Constants.Command command) {
+  /*Common properties among all commands*/
+  private Constants.Command command;            //Type of command from user
+
+  /*Properties used by Add and Edit*/
+  private String name;                          //Description of task
+  private DateTime dateTime;
+  
+  /*Display properties*/
+  private char displayOptions;                  //Pending or completed view of tasks
+  
+  /*Delete, done and edit properties*/
+  private int id;                               //Id associated with a task
+
+  //===========================================================================
+  // Private constructor
+  //===========================================================================
+  
+  /*Private contructor because only used by nested Builder class*/
+  private ParsedInput(
+    final Constants.Command command,
+    final int id,
+    final String name,
+    final DateTime dateTime, 
+    final char displayOptions) {
+
     this.command = command;
+    this.id = id;
+    this.name = name;
+    this.dateTime = dateTime;
+    this.displayOptions = displayOptions;
   }
-  
-  /*Returns command associated with this parsedInput*/
+
+  //===========================================================================
+  // Getters 
+  //===========================================================================
+
   public Constants.Command getCommand() {
     return command;
   }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public char getDisplayOptions() {
+    return displayOptions;
+  }
+
+  public DateTime getDateTime() {
+    return dateTime;
+  }
   
+  //===========================================================================
+  // Builder Class
+  //===========================================================================
+
+  public static class ParsedInputBuilder {
+    private final Constants.Command command;
+    private int id;
+    private String name;
+    private DateTime dateTime;
+    private char displayOptions;
+    
+    public ParsedInputBuilder(final Constants.Command command) {
+      this.command = command;
+    }
+    
+    public ParsedInputBuilder id(final int id) {
+      this.id = id;
+      return this;
+    }
+    
+    public ParsedInputBuilder name(final String name) {
+      this.name = name;
+      return this;
+    }
+    
+    public ParsedInputBuilder dateTime(final DateTime dateTime) {
+      this.dateTime = dateTime;
+      return this;
+    }
+    
+    public ParsedInputBuilder displayOptions(final char displayOptions) {
+      this.displayOptions = displayOptions;
+      return this;
+    }
+  }
 
 }
