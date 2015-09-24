081181b7039ed345d01f7b698dc33e706594ba48
jinifief <jinified@gmail.com>
2015-09-25 07:56:49 +0800

    Added simple parser

---
+++ src/raijin/logic/parser/SimpleParser.java
@@ -0,0 +1,10 @@
+package raijin.logic.parser;
+
+public class SimpleParser implements ParserInterface {
+
+  public ParsedInput parse(String userInput) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
