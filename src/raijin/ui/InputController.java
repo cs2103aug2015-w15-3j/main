911d6d5bc7c7c8350f52104d4adc2e21f0d4e415
woanchin <woanchin.tee@gmail.com>
2015-09-25 02:27:23 +0800

    added textfield bar and method to handle enter

--- src/raijin/ui/InputController.java
+++ src/raijin/ui/InputController.java
@@ -2,6 +2,37 @@ package raijin.ui;
 
 import javafx.scene.layout.BorderPane;
 
-public class InputController extends BorderPane {
+import java.io.IOException;
 
-}
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class InputController extends BorderPane {
+	@FXML
+	private TextField inputCommandBar;
+	
+	private static final String INPUT_COMMAND_BAR_LAYOUT_FXML = "resources/layout/InputController.fxml";
+	
+	private MainApplication mainApp;
+	
+	public InputController(MainApplication mainApp) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(INPUT_COMMAND_BAR_LAYOUT_FXML));
+		loader.setController(this);
+		loader.setRoot(this);
+		try {
+			loader.load();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		this.mainApp = mainApp;
+	}
+	
+	@FXML
+	public void onKeyPress(KeyEvent event) {
+		mainApp.handleKeyPress(this, event.getCode(), inputCommandBar.getText());
+	}
+	
+}	
