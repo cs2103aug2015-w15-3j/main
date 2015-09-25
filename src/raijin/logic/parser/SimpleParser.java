02735b3edbbbaa8dce07798f95a3ceece11dcf8d
LINGJIE-ZHU <zhulingjiee@gmail.com>
2015-09-25 23:02:25 +0800

    Further added on parseAdd without the last part.

--- src/raijin/logic/parser/SimpleParser.java
+++ src/raijin/logic/parser/SimpleParser.java
@@ -51,7 +51,16 @@ public class SimpleParser implements ParserInterface {
     return builder.createParsedInput();
   }
   
-
+  /**
+   * Method that modifies the attribute values of the ParseInputBuilder.
+   * 
+   * @param builder             builder to be modified
+   * @param id                  Task ID to be allocated for this task.
+   * @param taskName            Name of To-do.
+   * @param dateTime            Date and time of task.
+   * @param displayOptions      Display option.
+   * @return                    builder object back after inserting changing its attributes
+   */
   public ParsedInput.ParsedInputBuilder createBuilder(ParsedInput.ParsedInputBuilder builder, int id, 
       String taskName, DateTime dateTime, char displayOptions) {
     builder.id(id);
@@ -60,7 +69,6 @@ public class SimpleParser implements ParserInterface {
     builder.displayOptions(displayOptions);
     return builder;
   }
-  
   
   /**
    * Method that checks first String of String array for a particular word.
@@ -72,32 +80,97 @@ public class SimpleParser implements ParserInterface {
     return wordsOfInput[0].toLowerCase().equals(word.toLowerCase());
   }
   
+  /**
+   * Method that parses the input for any date or time inputs when task is added.
+   * Creates appropriate ParseInputBuilders accordingly.
+   */
   public void parseAddTask() {
-    boolean containsDate = false;
-    boolean containsTime = false;
-    int day, month, time;
+    boolean containsStartDate = false;
+    boolean containsEndDate = false;
+    boolean containsStartTime = false;
+    boolean containsEndTime = false;
+    int day, month, time; // For testing changing of string to integer format.
+    String startDate, startTime, endDate, endTime;
+    
     for (int i = 0; i < wordsOfInput.length - 1; i++) {
-      if (wordsOfInput[i].equalsIgnoreCase("by")) {
+      if (wordsOfInput[i].equalsIgnoreCase("by") || wordsOfInput[i].equalsIgnoreCase("on")) {
         if (wordsOfInput[i+1].contains("/")) {
-          String[] dayMonth = wordsOfInput[i+1].split("/");
+          // Checks for format of {startDate startTime}. If doesn't exist, ignore.
           try {
+            String[] dayMonth = wordsOfInput[i+1].split("/");
             day = Integer.parseInt(dayMonth[0]);
             month = Integer.parseInt(dayMonth[1]);
-            containsDate = true;
+            containsStartDate = true;
+            startDate = wordsOfInput[i+1];
           } catch (NumberFormatException e) {
           } catch (NullPointerException e) {
           }
           try {
             time = Integer.parseInt(wordsOfInput[i+2]);
-            containsTime = true;
+            containsStartTime = true;
+            startTime = wordsOfInput[i+2];
           } catch (NumberFormatException e) {
           } catch (NullPointerException e) {
           }
+          
+          if (containsStartDate && containsStartTime && i < wordsOfInput.length-5 && wordsOfInput[i+3].equalsIgnoreCase("to")) {
+            // startDate startTime endDate endTime
+            try {
+              String[] dayMonth = wordsOfInput[i+4].split("/");
+              day = Integer.parseInt(dayMonth[0]);
+              month = Integer.parseInt(dayMonth[1]);
+              containsEndDate = true;
+              startDate = wordsOfInput[i+4];
+            } catch (NumberFormatException e) {
+              // Invalid command?
+            } catch (NullPointerException e) {
+              // Invalid command?
+            }
+            try {
+              time = Integer.parseInt(wordsOfInput[i+5]);
+              containsEndTime = true;
+              endTime = wordsOfInput[i+5];
+            } catch (NumberFormatException e) {
+              // Invalid command?
+            }
+          } else if (containsStartDate && containsStartTime && i < wordsOfInput.length-4 && wordsOfInput[i+3].equalsIgnoreCase("to")) {
+            // startDate startTime endTime
+            try {
+              time = Integer.parseInt(wordsOfInput[i+4]);
+              containsEndTime = true;
+              endTime = wordsOfInput[i+4];
+            } catch (NumberFormatException e) {
+              // Invalid command?
+            }
+          }
         } else {
-          //TODO time
+          // Checks for format of {startTime}. If doesn't exist, ignore.
+          try {
+            time = Integer.parseInt(wordsOfInput[i+1]);
+            containsStartTime = true;
+          } catch (NumberFormatException e) {
+          } catch (NullPointerException e) {
+          }
         }
       }
     }
+    
+    // Creates relevant DateTime objects and then creates ParseInputBuilders.
+    if (containsStartDate && containsStartTime) {
+      if (containsEndDate && containsEndTime) {
+         
+        //createBuilder();
+      } else if (containsEndTime) {
+        
+        //createBuilder();
+      } else {
+      
+        //createBuilder();
+      } 
+    } else if (containsStartDate) {
+      
+      //createBuilder();
+    } 
   }
   
 }
