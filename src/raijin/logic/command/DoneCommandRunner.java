d8ddb538f0ed7bc1c7516569b1303247d3ec4ac2
woanchin <woanchin.tee@gmail.com>
2015-09-27 06:26:46 +0800

    Woan Chin defined execute method. It returns an encouraging message

--- src/raijin/logic/command/DoneCommandRunner.java
+++ src/raijin/logic/command/DoneCommandRunner.java
@@ -1,14 +1,25 @@
 package raijin.logic.command;
 
 import raijin.common.datatypes.Status;
+import raijin.common.datatypes.Task;
 import raijin.logic.api.CommandRunner;
 import raijin.logic.parser.ParsedInput;
+import raijin.storage.api.Memory;
 
 public class DoneCommandRunner implements CommandRunner {
-
-  public Status execute(ParsedInput input) {
-    // TODO Auto-generated method stub
-    return null;
+	Memory memory = Memory.getMemory();
+	int id;
+	String taskDescription;
+	
+  public Status execute(ParsedInput input) {   
+	this.id = input.getId();
+    Task task = memory.getTask(id);
+    taskDescription = task.getName();
+    
+	memory.addCompletedTask(task);
+    
+    return new Status("Nicely done! You have completed the task - " 
+    					+ taskDescription + 
+    					" Give yourself a pat on the back!");
   }
-
 }
