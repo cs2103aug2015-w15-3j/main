3e4468759b77eaa9d96968c2b5b3d3111747021e
Ying Jie <leeyingjie_95@hotmail.com>
2015-09-26 14:51:03 +0800

    Added a header text that shows today's date

--- src/raijin/ui/DisplayController.java
+++ src/raijin/ui/DisplayController.java
@@ -1,17 +1,26 @@
 package raijin.ui;
 
 //import javafx.application.Application;
-//import javafx.scene.control.Label;
-//import javafx.stage.Stage;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXML;
+
+import raijin.common.datatypes.Task;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class DisplayController extends BorderPane {
-	/*
-	 * Label test; //to be removed
-	 * 
-	 * public static void main(String[] args) { launch(args); }
-	 * 
-	 * @Override public void start(Stage stage) throws Exception { test = new
-	 * Label("test"); //to be removed stage.show(); }
-	 */
+	 
+	 final DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM ''yy");
+	 Date date;
+	 
+	 @FXML
+	 Label headMessage;
+	 
+	 public DisplayController() {
+		 date = new Date();
+		 headMessage = new Label("Tasks for " + dateFormat.format(date));
+		 this.setTop(headMessage);
+	 }
 }
