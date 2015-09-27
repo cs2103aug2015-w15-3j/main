ea3bef5ed8bcfaaa70fcd465ed800e048f1c3ba4
woanchin <woanchin.tee@gmail.com>
2015-09-27 18:56:22 +0800

    Woan Chin corrected undo and redo functions

--- src/raijin/logic/command/DeleteCommandRunner.java
+++ src/raijin/logic/command/DeleteCommandRunner.java
@@ -11,23 +11,24 @@ public class DeleteCommandRunner implements CommandRunner, UndoableRedoable {
 	Memory memory = Memory.getMemory();
 	int id;
 	String taskDescription;
+	Task task;
 	
   public Status execute(ParsedInput input) {   
 	this.id = input.getId();
-    Task task = memory.getTask(id);
+    this.task = memory.getTask(id);
     taskDescription = task.getName();
     
 	memory.deleteTask(id);
     
     return new Status("You have just deleted " + taskDescription + "!");
   }
-
+  
   public void undo() {
-    memory.undo();
+    	memory.addTask(task);
   }
 
   public void redo() {
-    memory.redo();
+    	memory.deleteTask(id);
   }
 
 }
