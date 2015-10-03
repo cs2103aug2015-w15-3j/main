1bc52d6680b806cf6cee6aea6cc0d5d2536a5a8b
Woan Chin <woanchin.tee@gmail.com>
2015-10-04 07:09:43 +0800

    Woan Chin saves data to a temporary file in another location before exiting the program

--- src/main/raijin/logic/command/ExitCommandRunner.java
+++ src/main/raijin/logic/command/ExitCommandRunner.java
@@ -1,5 +1,7 @@
 package raijin.logic.command;
 
+import java.io.IOException;
+
 import raijin.common.datatypes.Status;
 import raijin.logic.api.CommandRunner;
 import raijin.logic.parser.ParsedInput;
@@ -7,7 +9,13 @@ import raijin.logic.parser.ParsedInput;
 public class ExitCommandRunner extends CommandRunner {
 
   public Status processCommand(ParsedInput cmd) {
-    return new Status("Exiting");
+	  try {
+		  session.writeOnExit();
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
+	  
+	  return new Status("Exiting");
   }
 
 }
