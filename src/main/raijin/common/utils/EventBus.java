34ff97a68ee9943b79395ee0e340fc3987591bdf
jinifief <jinified@gmail.com>
2015-10-05 12:34:10 +0800

    Added initDisplayTasks

--- src/main/raijin/common/utils/EventBus.java
+++ src/main/raijin/common/utils/EventBus.java
@@ -1,9 +1,16 @@
 package raijin.common.utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.loadui.testfx.FXScreenController;
 
+import raijin.common.datatypes.DisplayContainer;
+import raijin.common.datatypes.ListDisplayContainer;
+import raijin.common.datatypes.Task;
+import raijin.storage.api.TasksManager;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
@@ -16,11 +23,16 @@ public class EventBus {
   private StringProperty feedBack = new SimpleStringProperty(this, "feedback", "Write here");
   private StringProperty displayHeadMessage = new SimpleStringProperty(this, "displayHeadMessage", "");
   private ObservableList<String> currentTasks = FXCollections.observableArrayList();
+  private DisplayContainer displayedTasks = new ListDisplayContainer();
   
   public String getFeedback() {
     return feedBack.get();
   }
     
+  public DisplayContainer getDisplayedTasks() {
+    return displayedTasks;
+  }
+
   public void setFeedback(String input) {
     feedBack.set(input);
   }
@@ -43,6 +55,15 @@ public class EventBus {
 
   public ObservableList<String> currentTasksProperty() {
     return currentTasks;
+  }
+
+  public void initDisplayTasks(TasksManager tasksManager) {
+    initCurrentTasks(tasksManager.getPendingTasks());
+  }
+
+  private void initCurrentTasks(HashMap<Integer, Task> pendingTasks) {
+    List<Task> tempList = new ArrayList<Task>(pendingTasks.values());
+    currentTasks.setAll(tempList.stream().map(Task::getName).collect(Collectors.toList()));
   }
 
   private EventBus() {}
