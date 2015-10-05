28ef6c27f0df89f1204adb6aa938892a30d77f22
jinifief <jinified@gmail.com>
2015-10-05 10:24:50 +0800

    Added observable list for current task

--- src/main/raijin/common/utils/EventBus.java
+++ src/main/raijin/common/utils/EventBus.java
@@ -1,12 +1,21 @@
 package raijin.common.utils;
 
+import java.util.List;
+
+import org.loadui.testfx.FXScreenController;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 
 public class EventBus {
 
   private static EventBus eventBus = new EventBus();
   private StringProperty feedBack = new SimpleStringProperty(this, "feedback", "Write here");
+  private StringProperty displayHeadMessage = new SimpleStringProperty(this, "displayHeadMessage", "");
+  private ObservableList<String> currentTasks = FXCollections.observableArrayList();
   
   public String getFeedback() {
     return feedBack.get();
@@ -16,8 +25,24 @@ public class EventBus {
     feedBack.set(input);
   }
 
+  public void setHeadMessage(String input) {
+    displayHeadMessage.set(input);
+  }
+
+  public void setCurrentTasks(List<String> tasks) {
+    currentTasks.setAll(tasks);
+  }
+
   public StringProperty feedBackProperty() {
     return feedBack;
+  }
+
+  public StringProperty displayHeadMessageProperty() {
+    return displayHeadMessage;
+  }
+
+  public ObservableList<String> currentTasksProperty() {
+    return currentTasks;
   }
 
   private EventBus() {}
