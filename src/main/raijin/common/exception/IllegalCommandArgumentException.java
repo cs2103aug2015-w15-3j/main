3c4aad2d81753b5482147520010ff7ab2468cc57
jinifief <jinified@gmail.com>
2015-10-05 20:35:33 +0800

    Change from List of Command Param to Command Param

--- src/main/raijin/common/exception/IllegalCommandArgumentException.java
+++ src/main/raijin/common/exception/IllegalCommandArgumentException.java
@@ -7,14 +7,14 @@ import raijin.common.datatypes.Constants;
 @SuppressWarnings("serial")
 public class IllegalCommandArgumentException extends RaijinException {
   
-  private List<Constants.CommandParam> arguments;
+  private Constants.CommandParam argument;
 
-  public IllegalCommandArgumentException(String message, List<Constants.CommandParam> args) {
+  public IllegalCommandArgumentException(String message, Constants.CommandParam arg) {
     super(message, Constants.Error.IllegalCommandArgument);
-    this.arguments = args;
+    this.argument = arg;
   }
 
-  public List<Constants.CommandParam> getArgumentsList() {
-    return arguments;
+  public Constants.CommandParam getArgument() {
+    return argument;
   }
 }
