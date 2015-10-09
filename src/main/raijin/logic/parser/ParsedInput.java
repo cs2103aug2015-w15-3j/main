64ae571a82e57ffb4f046327d94a12fd83eb3f27
jinifief <jinified@gmail.com>
2015-10-09 20:56:26 +0800

    Added build method for tag and priority

--- src/main/raijin/logic/parser/ParsedInput.java
+++ src/main/raijin/logic/parser/ParsedInput.java
@@ -14,18 +14,13 @@ public class ParsedInput {
   // Properties of Parsed Input 
   //===========================================================================
   
-  /*Common properties among all commands*/
-  private Constants.Command command;            //Type of command from user
-
-  /*Properties used by Add and Edit*/
-  private String name;                          //Description of task
+  private Constants.Command command;            
+  private int id = 0;                              
+  private String name;                             
   private DateTime dateTime;
-  
-  /*Display properties*/
-  private String displayOptions;                  //Pending or completed view of tasks
-  
-  /*Delete, done and edit properties*/
-  private int id = 0;                               //Id associated with a task
+  private String displayOptions;                  
+  private String tag;
+  private char priority = 'm';              //Default to medium priority
 
   //===========================================================================
   // Private constructor
@@ -37,13 +32,17 @@ public class ParsedInput {
     final int id,
     final String name,
     final DateTime dateTime, 
-    final String displayOptions) {
+    final String displayOptions, 
+    final String tag,
+    final char priority) {
 
     this.command = command;
     this.id = id;
     this.name = name;
     this.dateTime = dateTime;
     this.displayOptions = displayOptions;
+    this.tag = tag;
+    this.priority = priority;
   }
 
   //===========================================================================
@@ -66,14 +65,22 @@ public class ParsedInput {
     return name;
   }
 
-  public String getDisplayOptions() {
-    return displayOptions;
-  }
-
   public DateTime getDateTime() {
     return dateTime;
   }
+
+  public String getDisplayOptions() {
+    return displayOptions;
+  }
   
+  public String getTag() {
+    return tag;
+  }
+
+  public char getPriority() {
+    return priority;
+  }
+
   //===========================================================================
   // Builder Class
   //===========================================================================
@@ -84,6 +91,8 @@ public class ParsedInput {
     private String name;
     private DateTime dateTime;
     private String displayOptions;
+    private String tag;
+    private char priority;
     
     public ParsedInputBuilder(final Constants.Command command) {
       this.command = command;
@@ -109,9 +118,20 @@ public class ParsedInput {
       return this;
     }
     
+    public ParsedInputBuilder tag(final String tag) {
+      this.tag = tag;
+      return this;
+    }
+    
+    public ParsedInputBuilder priority(final char priority) {
+      this.priority = priority;
+      return this;
+    }
+
     /*Generate parsedinput when client is done building the object*/
     public ParsedInput createParsedInput() {
-      return new ParsedInput(command, id, name, dateTime, displayOptions);
+      return new ParsedInput(command, id, name, dateTime, displayOptions, 
+                             tag, priority);
     }
 
   }
