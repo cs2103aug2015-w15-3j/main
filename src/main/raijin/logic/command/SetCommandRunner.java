6affe3459ab3d0bc4d575b3c3eced997aad21449
jinifief <jinified@gmail.com>
2015-10-17 07:33:13 +0800

    Fix invalid path given by user

--- src/main/raijin/logic/command/SetCommandRunner.java
+++ src/main/raijin/logic/command/SetCommandRunner.java
@@ -5,16 +5,23 @@ import raijin.common.datatypes.Status;
 import raijin.common.exception.UnableToExecuteCommandException;
 import raijin.logic.api.CommandRunner;
 import raijin.logic.parser.ParsedInput;
+import raijin.storage.handler.StorageHandler;
 
 public class SetCommandRunner extends CommandRunner {
 
-  private final String SUCCESS_MESSAGE = "Your data is now safe at %s";
+  final String SUCCESS_MESSAGE = "Your data is now safe at %s";
+  final String FAIL_MESSAGE = "You have provided an invalid path. Please"
+      + " make sure that the folder exists";
 
   @Override
   protected Status processCommand(ParsedInput input) throws UnableToExecuteCommandException {
-    String storagePath = input.getHelperOption() + Constants.NAME_USER_FOLDER;
-    session.setStorageDirectory(storagePath, session.baseConfigPath);
-    return new Status(String.format(SUCCESS_MESSAGE, storagePath));
+    String storagePath = input.getHelperOption();
+    if (StorageHandler.isDirectory(storagePath)) {          // Check whether directory exists
+      storagePath = input.getHelperOption() + Constants.NAME_USER_FOLDER;
+      session.setStorageDirectory(storagePath, session.baseConfigPath);
+      return new Status(String.format(SUCCESS_MESSAGE, storagePath));
+    }
+    return new Status(FAIL_MESSAGE);            
   }
 
 }
--- src/test/raijin/logic/command/SetCommandRunnerTest.java
+++ src/test/raijin/logic/command/SetCommandRunnerTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import raijin.common.datatypes.Constants;
+import raijin.common.datatypes.Status;
 import raijin.common.exception.UnableToExecuteCommandException;
 import raijin.logic.parser.ParsedInput;
 import raijin.logic.parser.ParsedInputTest;
@@ -23,15 +24,23 @@ public class SetCommandRunnerTest {
   }
 
   @Test
-  public void processCommand_ChangeStoragePath() throws UnableToExecuteCommandException {
+  public void processCommand_ValidPath_ChangeStoragePath() throws UnableToExecuteCommandException {
     String initialStoragePath = session.storageDirectory;
     ParsedInput input = new ParsedInput.ParsedInputBuilder(Constants.Command.SET).
         helperOption("/tmp").createParsedInput();
    
     setCommandRunner.execute(input);
     String currentStoragePath = session.storageDirectory;
+    System.out.println(initialStoragePath);
     assertNotEquals(initialStoragePath, currentStoragePath);
-
   }
 
+  @Test
+  public void processCommand_InvalidPath_ReturnFail() throws UnableToExecuteCommandException {
+    ParsedInput input = new ParsedInput.ParsedInputBuilder(Constants.Command.SET).
+        helperOption("/tmp/holland").createParsedInput();
+   
+    Status status = setCommandRunner.execute(input);
+    assertEquals(setCommandRunner.FAIL_MESSAGE, status.getFeedback());
+  }
 }
