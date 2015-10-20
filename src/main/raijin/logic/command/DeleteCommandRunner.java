c6a7eacf79109e48107aa6feb1a5c87326d440d4
jinifief <jinified@gmail.com>
2015-10-20 08:37:08 +0800

    Delete based on tag

--- src/main/raijin/logic/command/DeleteCommandRunner.java
+++ src/main/raijin/logic/command/DeleteCommandRunner.java
@@ -1,8 +1,10 @@
 package raijin.logic.command;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import raijin.common.datatypes.Constants;
 import raijin.common.datatypes.Status;
@@ -12,6 +14,7 @@ import raijin.logic.parser.ParsedInput;
 import raijin.common.datatypes.Task;
 import raijin.common.exception.NoSuchTaskException;
 import raijin.common.exception.UnableToExecuteCommandException;
+import raijin.common.utils.TaskUtils;
 
 public class DeleteCommandRunner extends CommandRunner implements  UndoableRedoable {
   TreeSet<Integer> idsToDone = new TreeSet<Integer>();
@@ -24,6 +27,10 @@ public class DeleteCommandRunner extends CommandRunner implements  UndoableRedoa
   public Status processCommand(ParsedInput input) throws UnableToExecuteCommandException {
     idsToDone = input.getIds();
     
+    if (idsToDone.isEmpty()) {
+      idsToDone = getIdsFromTags(input.getTags());
+    }
+
     while(!idsToDone.isEmpty()) {
       int id = idsToDone.pollFirst();
       try {
@@ -74,6 +81,20 @@ public class DeleteCommandRunner extends CommandRunner implements  UndoableRedoa
         wrapLowerLevelException(e, Constants.Command.DELETE);
       }
     }
+  }
+
+
+  /**
+   * Get id from filtered tasks when tag is chosen 
+   * @param tags
+   * @return
+   */
+  TreeSet<Integer> getIdsFromTags(TreeSet<String> tags) {
+    List<Task> filtered = TaskUtils.filterTaskWithTags(tasksManager.getPendingTasks(), 
+        tags);
+    System.out.println(filtered.toString());
+    return new TreeSet<Integer>(filtered.stream().map(
+        t -> t.getId()).collect(Collectors.toList()));
   }
 
 }
