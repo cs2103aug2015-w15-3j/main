d57cd5e64ea4c8736538fb0ea75124b211379276
jinifief <jinified@gmail.com>
2015-10-23 03:41:56 +0800

    Added priority filter

---
+++ src/main/raijin/common/utils/filter/PriorityFilter.java
@@ -0,0 +1,22 @@
+package raijin.common.utils.filter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import raijin.common.datatypes.Task;
+
+public class PriorityFilter extends TaskFilter {
+
+  private String priority;
+  
+  public PriorityFilter(String priority) {
+    this.priority = priority;
+  }
+
+  @Override
+  public List<Task> filter(List<Task> tasks) {
+    return tasks.stream().filter(task -> task.getPriority().equals(priority)).
+        collect(Collectors.toList());
+  }
+
+}
