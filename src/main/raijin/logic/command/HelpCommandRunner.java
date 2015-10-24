e798a4f8a2a7a024531322f49b08fda4e553a71f
Woan Chin <woanchin.tee@gmail.com>
2015-10-25 04:14:39 +0800

    woan implemented an ugly buttttt super basic help command

--- src/main/raijin/logic/command/HelpCommandRunner.java
+++ src/main/raijin/logic/command/HelpCommandRunner.java
@@ -1,14 +1,25 @@
 package raijin.logic.command;
 
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+
 import raijin.common.datatypes.Status;
 import raijin.logic.api.CommandRunner;
 import raijin.logic.parser.ParsedInput;
 import raijin.common.datatypes.Constants;
+import raijin.common.eventbus.events.SetCurrentDisplayEvent;
+import raijin.common.eventbus.RaijinEventBus;
+
 
 public class HelpCommandRunner extends CommandRunner {
-
+  private EventBus eventBus = RaijinEventBus.getEventBus();
+  
   public Status processCommand(ParsedInput cmd) {
-    return new Status(Constants.FEEDBACK_INFO_SUCCESS, Constants.HELP_MESSAGE);
+	  String message = "Help!";
+
+    eventBus.post(new SetCurrentDisplayEvent(Constants.HELP_MESSAGE, message));
+	
+    return new Status(Constants.FEEDBACK_INFO_SUCCESS);
   }
 
 }
