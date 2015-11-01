956af60b729b57b99e302d45d1f81234c77159eb
jinifief <jinified@gmail.com>
2015-11-01 20:06:42 +0800

    Added free time slot

--- src/main/raijin/common/datatypes/TimeSlot.java
+++ src/main/raijin/common/datatypes/TimeSlot.java
@@ -3,6 +3,7 @@ package raijin.common.datatypes;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -35,6 +36,8 @@ public class TimeSlot {
     occupiedSlots = events.stream().map(t -> t.getDateTime()).collect(
         Collectors.toList());
     Collections.sort(occupiedSlots, durationComparator);
+    streamlineEvents();
+    System.out.println("DONE");
   }
 
   List<Task> filterEvents(List<Task> pendingTasks) {
@@ -51,13 +54,86 @@ public class TimeSlot {
     return events;
   }
   
-  List<DateTime> getOccupiedSlots() {
+  public List<DateTime> getOccupiedSlots() {
     return occupiedSlots;
+  }
+
+  List<DateTime> streamlineEvents() {
+    List<DateTime> filtered = new ArrayList<DateTime>();
+    
+    for (DateTime source : occupiedSlots) {
+      if (source != null && occupiedSlots.indexOf(source)
+          != occupiedSlots.size() -1) {
+        filtered.add(iterativeExtendDuration(source));
+      }
+    }
+
+    return filtered;
   }
 
   long getDuration(LocalTime start, LocalTime end) {
     long minute = start.until(end, ChronoUnit.MINUTES);
     return minute;
+  }
+
+
+  DateTime iterativeExtendDuration(DateTime source) {
+    DateTime result = source;
+    int index = occupiedSlots.indexOf(source);
+
+    for (int i = index + 1 ; i < occupiedSlots.size() ; i++) {
+      DateTime target = occupiedSlots.get(i);
+      System.out.println(result.getStartTime().toString() + " " 
+          + result.getEndTime().toString());
+      if (target != null) {
+        result = extendDuration(result, target);
+      }
+    }
+    return result;
+  }
+
+  //Extends duration of source if overlap occur with target
+  DateTime extendDuration(DateTime source, DateTime target) {
+
+    int compareStartWithEnd = source.getStartTime().compareTo(target.getEndTime());
+    int compareEndWithStart = source.getEndTime().compareTo(target.getStartTime());
+    int compareStartWithStart = source.getStartTime().compareTo(target.getStartTime());
+    int compareEndWithEnd = source.getEndTime().compareTo(target.getEndTime());
+
+    if (isNoOverlap(compareStartWithEnd, compareEndWithStart)) {
+      return source;
+    } else if (isWithinDuration(compareStartWithStart, compareEndWithEnd)) {
+      occupiedSlots.set(occupiedSlots.indexOf(target), null);
+      return source;
+    } else {
+      occupiedSlots.set(occupiedSlots.indexOf(target), null);
+      return handleOverlapEvents(compareStartWithStart, compareEndWithEnd, source, target);
+    }
+  }
+  
+  DateTime handleOverlapEvents(int compareStartWithStart, int compareEndWithEnd,
+      DateTime source, DateTime target) {
+    LocalTime startTime = source.getStartTime();
+    LocalTime endTime = source.getEndTime();
+
+    if (compareStartWithStart > 0) {
+      startTime = target.getStartTime();
+    } 
+
+    if (compareEndWithEnd < 0) {
+      endTime = target.getEndTime();
+    }
+    return new DateTime(startTime, endTime);
+  }
+
+  //Check if there is any overlap between two events
+  boolean isNoOverlap(int compareStartWithEnd, int compareEndWithStart) {
+    return compareStartWithEnd > 0 || compareEndWithStart < 0;
+  }
+  
+  //Checks if the event lies within another event
+  boolean isWithinDuration(int compareStartWithStart, int compareEndWithEnd) {
+    return compareStartWithStart <= 0 && compareEndWithEnd >= 0;
   }
 
 }
