93d8463a29995fc81a48f15e2e6f7efb8ed64412
jinifief <jinified@gmail.com>
2015-11-04 20:27:59 +0800

    Added comments

--- src/main/raijin/common/datatypes/DateTime.java
+++ src/main/raijin/common/datatypes/DateTime.java
@@ -1,4 +1,4 @@
-//@@author A0112213E
+// @@author A0112213E
 
 package raijin.common.datatypes;
 
@@ -10,24 +10,19 @@ import raijin.common.utils.RaijinLogger;
 
 public class DateTime implements Comparable<DateTime> {
 
-  private static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(Constants.FORMAT_DATE);
-  private static DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(Constants.FORMAT_TIME);
+  private static DateTimeFormatter dateFormatter = DateTimeFormatter
+      .ofPattern(Constants.FORMAT_DATE);
+  private static DateTimeFormatter timeFormatter = DateTimeFormatter
+      .ofPattern(Constants.FORMAT_TIME);
+
   private LocalDate startDate;
   private LocalDate endDate;
   private LocalTime startTime;
   private LocalTime endTime;
-  
-  //===========================================================================
-  // Uses Java Time API for construction
-  //===========================================================================
-  
-  public DateTime(LocalDate startDate, LocalTime startTime, LocalDate endDate, 
-      LocalTime endTime) {
-    this.startDate = startDate;
-    this.startTime = startTime;
-    this.endDate = endDate;
-    this.endTime = endTime;
-  }
+
+  // =========================
+  // Constructors for testing
+  // =========================
 
   public DateTime(LocalDate startDate, LocalDate endDate) {
     this.startDate = startDate;
@@ -39,43 +34,60 @@ public class DateTime implements Comparable<DateTime> {
     this.endTime = endTime;
   }
 
-  //===========================================================================
-  // Specific deadline
-  //===========================================================================
-
-  /*Create deadline with specific date*/
-  public DateTime(String endDate){
-    this.startDate = LocalDate.parse(endDate, dateFormatter);
-    this.endDate = LocalDate.parse(endDate, dateFormatter);   //When no end date provided, same as start date
-    this.endTime = LocalTime.of(23,59);                       //Default to 2359 when no time specified
+  public DateTime(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+    this.startDate = startDate;
+    this.startTime = startTime;
+    this.endDate = endDate;
+    this.endTime = endTime;
   }
 
-  /*Create deadline with specific date and time*/
-  public DateTime(String endDate, String endTime){
+
+  // ============================
+  // Constructors used by Parser
+  // ============================
+
+  /**
+   * Constructs deadline when only end date is specified
+   * @param endDate
+   */
+  public DateTime(String endDate) {
     this.startDate = LocalDate.parse(endDate, dateFormatter);
-    this.endDate = LocalDate.parse(endDate, dateFormatter);   //When no end date provided, same as start date
-    this.endTime = LocalTime.parse(endTime, timeFormatter);               
+    this.endDate = LocalDate.parse(endDate, dateFormatter);
+    /* Set time to 2359 when not specified by user */
+    this.endTime = LocalTime.of(23, 59);
   }
 
-  //===========================================================================
-  // Event
-  //===========================================================================
-
-  /*Create event deadline*/
-  public DateTime(String endDate, String startTime, String endTime){
+  /**
+   * Constructs deadline when both time and date is specified
+   * @param endDate
+   * @param endTime
+   */
+  public DateTime(String endDate, String endTime) {
+    /* Set start date to end date to avoid deadling with null */
     this.startDate = LocalDate.parse(endDate, dateFormatter);
-    this.endDate = LocalDate.parse(endDate, dateFormatter);   
-    this.startTime = LocalTime.parse(startTime, timeFormatter);               
-    this.endTime = LocalTime.parse(endTime, timeFormatter);               
+    this.endDate = LocalDate.parse(endDate, dateFormatter);
+    this.endTime = LocalTime.parse(endTime, timeFormatter);
   }
 
-  /*Creates event that spans multiple days*/
-  public DateTime(String startDate, String startTime, 
-      String endDate, String endTime){
+  /**
+   * Constructs deadline for task that spans certain duration on the same day
+   * @param endDate
+   * @param startTime
+   * @param endTime
+   */
+  public DateTime(String endDate, String startTime, String endTime) {
+    this.startDate = LocalDate.parse(endDate, dateFormatter);
+    this.endDate = LocalDate.parse(endDate, dateFormatter);
+    this.startTime = LocalTime.parse(startTime, timeFormatter);
+    this.endTime = LocalTime.parse(endTime, timeFormatter);
+  }
+
+  /* Creates event that spans multiple days */
+  public DateTime(String startDate, String startTime, String endDate, String endTime) {
     this.startDate = LocalDate.parse(startDate, dateFormatter);
-    this.endDate = LocalDate.parse(endDate, dateFormatter);   
-    this.startTime = LocalTime.parse(startTime, timeFormatter);               
-    this.endTime = LocalTime.parse(endTime, timeFormatter);               
+    this.endDate = LocalDate.parse(endDate, dateFormatter);
+    this.startTime = LocalTime.parse(startTime, timeFormatter);
+    this.endTime = LocalTime.parse(endTime, timeFormatter);
   }
 
   public LocalDate getEndDate() {
@@ -93,12 +105,26 @@ public class DateTime implements Comparable<DateTime> {
   public LocalTime getStartTime() {
     return startTime;
   }
-  
+
+  /**
+   * Handles comparison of null fields
+   * @param source 
+   * @param target
+   * @return
+   */
+  boolean compare(Object source, Object target) {
+    if (source == null) {
+      return target == null;
+    } else {
+      return source.equals(target);
+    }
+  }
+
   @Override
   public boolean equals(Object ob2) {
     if (ob2 instanceof DateTime) {
       return compare(startDate, ((DateTime) ob2).getStartDate())
-          && compare(endDate, ((DateTime) ob2).getEndDate()) 
+          && compare(endDate, ((DateTime) ob2).getEndDate())
           && compare(startTime, ((DateTime) ob2).getStartTime())
           && compare(endTime, ((DateTime) ob2).getEndTime());
     }
@@ -107,51 +133,7 @@ public class DateTime implements Comparable<DateTime> {
 
 
   /**
-   * Compare only start date, if same start date is the same
-   * Compare time using the same manner 
-   * Compare end date, null is sorted first  
-   * @param DateTime compared
-   * @return
-   */
-
-  @Override
-  public int compareTo(DateTime compared) {
-    int result = getEndDate().compareTo(compared.getEndDate());
-
-    //When both end dates are different
-    if (result != 0) {
-      return result;
-    } else {
-      result = getStartDate().compareTo(compared.getStartDate());
-      
-      //When both start dates are different
-      if (result != 0) {
-        return result;
-      } else {
-        result = getEndTime().compareTo(compared.getEndTime());
-
-        //When both end times are different 
-        if (result != 0) {
-          return result;
-        } else {
-          return compareStartTime(getStartTime(), compared.getStartTime());
-        }
-      }
-    }
-      
-    
-  }
-
-  boolean compare(Object source, Object target) {
-    if (source == null) {
-      return target == null;
-    } else {
-      return source.equals(target);
-    }
-  }
-  
-  /**
-   * Compare time when both shared the same start time
+   * Compare time when both shared the same end time.
    * @param source
    * @param target
    * @return
@@ -166,6 +148,41 @@ public class DateTime implements Comparable<DateTime> {
     } else {
       return source.compareTo(target);
     }
+  }
+
+  /**
+   * Comparator for DateTime objects
+   * 
+   * @param DateTime compared
+   * @return
+   */
+
+  @Override
+  public int compareTo(DateTime compared) {
+    int result = getEndDate().compareTo(compared.getEndDate());
+
+    // When both end dates are different
+    if (result != 0) {
+      return result;
+    } else {
+      result = getStartDate().compareTo(compared.getStartDate());
+
+      // When both start dates are different
+      if (result != 0) {
+        return result;
+      } else {
+        result = getEndTime().compareTo(compared.getEndTime());
+
+        // When both end times are different
+        if (result != 0) {
+          return result;
+        } else {
+          return compareStartTime(getStartTime(), compared.getStartTime());
+        }
+      }
+    }
+
+
   }
 
 
