718a01c3ab648c6f95c2c6965b45db3007005144
jinifief <jinified@gmail.com>
2015-11-04 23:02:11 +0800

    commented

--- src/main/raijin/common/eventbus/events/ScrollEvent.java
+++ src/main/raijin/common/eventbus/events/ScrollEvent.java
@@ -4,7 +4,7 @@ package raijin.common.eventbus.events;
 
 public class ScrollEvent {
   
-  public int scrollDelta = 10;        //Scroll through 10 items
+  public int scrollDelta = 10;        //Scroll through 10 items at a time
   
   public ScrollEvent(int direction) {
     scrollDelta*=direction;
