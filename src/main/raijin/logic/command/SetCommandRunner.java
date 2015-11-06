51d3812c15b5d9d342ca1045e607b69baed69915
jinifief <jinified@gmail.com>
2015-11-06 13:52:44 +0800

    added comments

--- src/main/raijin/logic/command/SetCommandRunner.java
+++ src/main/raijin/logic/command/SetCommandRunner.java
@@ -9,16 +9,23 @@ import raijin.logic.api.CommandRunner;
 import raijin.logic.parser.ParsedInput;
 import raijin.storage.handler.StorageHandler;
 
+/**
+ * Sets storage directory specified by user
+ * @author papa
+ *
+ */
 public class SetCommandRunner extends CommandRunner {
 
-  final String SUCCESS_MESSAGE = "Your data is now safe at %s";
-  final String FAIL_MESSAGE = "You have provided an invalid path. Please"
+  static final String SUCCESS_MESSAGE = "Your data is now safe at %s";
+  static final String FAIL_MESSAGE = "You have provided an invalid path. Please"
       + " make sure that the folder exists";
 
   @Override
   protected Status processCommand(ParsedInput input) throws UnableToExecuteCommandException {
+    /*Obtain path of directory*/
     String storagePath = input.getHelperOption();
-    if (StorageHandler.isDirectory(storagePath)) {          // Check whether directory exists
+
+    if (StorageHandler.isDirectory(storagePath)) {          
       storagePath = input.getHelperOption() + Constants.NAME_USER_FOLDER;
       session.setStorageDirectory(storagePath, session.baseConfigPath);
       return new Status(String.format(SUCCESS_MESSAGE, storagePath));
