623e2b934963f1c6a3a40c906811d0a9d5d80a7f
jinifief <jinified@gmail.com>
2015-11-09 11:40:18 +0800

    Change message to include priority

--- src/main/raijin/logic/command/SearchCommandRunner.java
+++ src/main/raijin/logic/command/SearchCommandRunner.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
 
+import raijin.common.datatypes.Constants;
 import raijin.common.datatypes.Status;
 import raijin.common.datatypes.Task;
 import raijin.common.eventbus.events.SetCurrentDisplayEvent;
@@ -26,7 +27,7 @@ import raijin.logic.parser.ParsedInput;
 public class SearchCommandRunner extends CommandRunner {
 
   private static final String DISPLAY_MESSAGE = "Search results: %d found";
-  private static final String INITIAL_FEEDBACK_MESSAGE = "Search keywords: ";
+  private static final String INITIAL_FEEDBACK_MESSAGE = "Search: ";
   private static final String MESSAGE_TEMPLATE = "\"%s\", ";
   String displayMessageSent;            //Search result message displayed  
 
@@ -50,24 +51,65 @@ public class SearchCommandRunner extends CommandRunner {
     currentTask = new Task(input.getName(), idManager.getId(), input);
   }
 
-  Status createSuccessfulStatus(ArrayList<String> keywords) {
-    StringBuilder strBuilder = new StringBuilder(INITIAL_FEEDBACK_MESSAGE);
-
+  public void appendKeywords(ArrayList<String> keywords, StringBuilder strBuilder) {
     for (String keyword : keywords) {
       if (!keyword.equals("")) {
         strBuilder.append(String.format(MESSAGE_TEMPLATE, keyword));
       }
     }
+  }
 
+  public void appendTags(StringBuilder strBuilder) {
     for (String tag : currentTask.getTags()) {
       strBuilder.append(String.format(MESSAGE_TEMPLATE, "#" + tag));
     }
+  }
 
-    String feedback = strBuilder.toString();
-    feedback = feedback.substring(0, feedback.length()-2);
-    feedback = normalizeTaskName(feedback);
+  /**
+   * Get full string representation of a priority
+   * @param priority
+   * @return
+   */
+  String getFullPriority(String priority) {
+    String fullPriority = "Medium priority";
+    if (priority.equals(Constants.PRIORITY_HIGH)) {
+      fullPriority = "High priority";
+    } else if (priority.equals(Constants.PRIORITY_LOW)) {
+      fullPriority = "Low priority";
+    }
+    return fullPriority;
+  }
+
+  String appendPriority(String feedback) {
+    if (inputPriority != null) {
+      return feedback + " " + getFullPriority(inputPriority);
+    }
+    return feedback;
+  }
+
+  String createFeedbackMessage(ArrayList<String> keywords, StringBuilder strBuilder) {
+    appendKeywords(keywords, strBuilder);
+    appendTags(strBuilder);
+    String result = strBuilder.toString();
+    /*Checks if any message is added to string builder*/
+    if (!result.equals(INITIAL_FEEDBACK_MESSAGE)) {
+      /*Removes hanging comma*/
+      result = result.substring(0, result.length()-2);
+    }
+    result = appendPriority(result);
+    return normalizeTaskName(result);
+  }
+
+
+  Status createSuccessfulStatus(ArrayList<String> keywords) {
+    StringBuilder strBuilder = new StringBuilder(INITIAL_FEEDBACK_MESSAGE);
+    /*Generates feedback message*/
+    String feedback = createFeedbackMessage(keywords, strBuilder);
     return new Status(feedback);
   }
+
+
+
 
   /*checks if the task matches tag query*/
   public boolean matchOnlyTags(Task task, TreeSet<String> tags) {
